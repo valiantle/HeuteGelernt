5a8a721d8e312731d5ef2047708bed1facca3fce
valiantleprano <valiant.leprano@gmail.com>
2025-02-13 20:54:25 +0000

    15.menimdead

---
+++ day14/new.js
@@ -0,0 +1,28 @@
+let age = 150;
+
+
+if (age >= 18) {
+    console.log("Du bist volljahrig");
+} else if (age === 17) {
+    console.log("Du bist 17 Jhare alt");
+} else {
+    console.log("Du bist minderjahrig");
+}
+
+
+let score = -69
+
+//90 + Sehr gut
+//75 + Gut
+//50 + Befriedigend
+//<50 nicht bestanden
+
+if (score >= 90) {
+    console.log("Sehr gut!!!!11");
+} else if (score >= 75 && score < 90) {
+    console.log("Solid OK");
+} else if (score >= 50 && score < 75) {
+    console.log("Pls try better next time...");
+} else  {
+    console.log("BRUH");
+}
